d225125395c1caf7ec2b879b7ca740eacd30d810
samlovelace <samlovelace12@gmail.com>
2025-05-06 22:21:02 -0400

    updates to mount manip on table, removes spawning of urdf manip

--- launch/other.py
+++ launch/other.py
@@ -9,16 +9,4 @@ def generate_launch_description():
             cmd=['ign', 'gazebo', '-v', '4', '-r', '/home/sam/dev/cpp/arm_ws/install/arm_simulator/share/arm_simulator/worlds/empty.world'],
             output='screen'
         ),
-
-        # Spawn your robot model in Ignition Gazebo
-        Node(
-            package='ros_gz_sim',
-            executable='create',
-            arguments=[
-                '-name', 'my_robot',
-                '-file', '/home/sam/dev/cpp/arm_ws/install/arm_simulator/share/arm_simulator/urdf/manipulator.urdf',
-                '-x', '0.0', '-y', '0.0', '-z', '1.015'
-            ],
-            output='screen'
-        )
     ])
