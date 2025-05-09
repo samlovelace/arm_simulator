f7a5e653f0aa55d8f9112059d6a75b7d6f5b26b9
samlovelace <samlovelace12@gmail.com>
2025-05-08 20:48:00 -0400

    clean up files and rename urdf and world files for UR manip

--- launch/UR_launch.py
+++ launch/UR_launch.py
@@ -6,7 +6,7 @@ def generate_launch_description():
     return LaunchDescription([
         # Start Gazebo Sim (Ignition)
         ExecuteProcess(
-            cmd=['ign', 'gazebo', '-v', '4', '-r', '/home/sam/dev/cpp/arm_ws/install/arm_simulator/share/arm_simulator/worlds/empty.world'],
+            cmd=['ign', 'gazebo', '-v', '4', '-r', '/home/sam/dev/cpp/arm_ws/install/arm_simulator/share/arm_simulator/worlds/UR.world'],
             output='screen'
         ),
     ])
