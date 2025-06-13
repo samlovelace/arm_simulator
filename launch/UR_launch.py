ce759d56cc9b96042e55e9f29f1c4593ec08cabb
samlovelace <samlovelace12@gmail.com>
2025-06-13 18:29:57 -0400

    updates launch file to dynamically determine the path to world file

--- launch/UR_launch.py
+++ launch/UR_launch.py
@@ -1,12 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    
+    world_path = os.path.join(
+        get_package_share_directory('arm_simulator'),
+        'worlds',
+        'UR.world'
+    )
+
     return LaunchDescription([
         # Start Gazebo Sim (Ignition)
         ExecuteProcess(
-            cmd=['ign', 'gazebo', '-v', '4', '-r', '/home/sam/dev/cpp/arm_ws/install/arm_simulator/share/arm_simulator/worlds/UR.world'],
+            cmd=['ign', 'gazebo', '-v', '4', '-r', world_path],
             output='screen'
         ),
     ])
