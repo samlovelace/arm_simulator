1984c416bf8ab970d372a305b6bc6b62aab9e0f8
samlovelace <samlovelace12@gmail.com>
2025-07-05 20:15:16 -0400

    updates folder structure to ease compiling multiple plugins

---
+++ plugins/posePublisher/PosePublisher.hpp
@@ -0,0 +1,52 @@
+#include <ignition/gazebo/System.hh>
+#include <ignition/gazebo/Model.hh>
+#include <ignition/gazebo/components/Joint.hh>
+#include <ignition/gazebo/components/JointPosition.hh>
+#include <ignition/plugin/Register.hh>
+#include <ignition/gazebo/Joint.hh>
+
+#include <rclcpp/rclcpp.hpp>
+#include "nora_idl/msg/robot_state.hpp"
+
+#include <thread>
+#include <memory> 
+#include <mutex> 
+#include "RateController.h"
+
+class PosePublisher
+    : public ignition::gazebo::System,
+      public ignition::gazebo::ISystemConfigure,
+      public ignition::gazebo::ISystemPostUpdate
+{
+public:
+	PosePublisher() = default;
+
+	void Configure(const ignition::gazebo::Entity &entity,
+					const std::shared_ptr<const sdf::Element> &,
+					ignition::gazebo::EntityComponentManager &ecm,
+					ignition::gazebo::EventManager &) override;
+
+	void PostUpdate(const ignition::gazebo::UpdateInfo &_info,
+                    const ignition::gazebo::EntityComponentManager &_ecm) override;
+
+	~PosePublisher() override;
+
+private:
+
+	ignition::gazebo::Model mModel{ignition::gazebo::kNullEntity};
+	// std::shared_ptr<rclcpp::Node> mRosNode;
+	// rclcpp::Publisher<nora_idl::msg::RobotState>::SharedPtr mPosPub; 
+	// std::thread mRosSpinThread;
+	// std::thread mPublishThread; 
+	// std::unique_ptr<RateController> mPublishRate; 
+
+    // void PosePublisher::robotStatePublishLoop(ignition::gazebo::EntityComponentManager& ecm);
+
+};
+
+// Plugin registration
+IGNITION_ADD_PLUGIN(
+    PosePublisher,
+    ignition::gazebo::System,
+    ignition::gazebo::ISystemConfigure,
+    ignition::gazebo::ISystemPostUpdate)
